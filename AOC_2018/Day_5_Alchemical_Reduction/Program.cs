92c08d854f3acdf66dbc4a735b812a2d5bee9292
Matthew Duffy <duffym22@gmail.com>
2018-12-09 20:10:17 -0600

    - Add Day 5 - Rename Day 1 folder and updated solution

---
+++ AOC_2018/Day_5_Alchemical_Reduction/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_5_Alchemical_Reduction
+{
+    class Program
+    {
+        static List<string> lines = new List<string>();
+        static string _FILE = @"C:\Users\Matthew\source\repos\AOC_2018\AOC_2018\Day_5_Alchemical_Reduction\Day5.txt";
+
+        static void Main(string[] args)
+        {
+            Read_Input();
+        }
+
+        static void Read_Input()
+        {
+            using (StreamReader reader = new StreamReader(_FILE))
+            {
+                while (!reader.EndOfStream)
+                {
+                    lines.Add(reader.ReadLine());
+                }
+            }
+        }
+    }
+}
