7c40ec26ff883e6fbf1debe3057c2095e80a09d7
Matthew Duffy <duffym22@gmail.com>
2018-12-09 18:51:47 -0600

    Tried recursion - but doesnt go any lower. Going back to try looping through claims and through the grid

--- AOC_2018/Day_3_No_Matter_How_You_Slice_It/Program.cs
+++ AOC_2018/Day_3_No_Matter_How_You_Slice_It/Program.cs
@@ -5,106 +5,180 @@ using System.IO;
 
 namespace Day_3_No_Matter_How_You_Slice_It
 {
-   class Program
-   {
-      static List<string> lines = new List<string>();
-      static List<Rectangle> rekt = new List<Rectangle>();
-      static List<Rectangle> overlapRect = new List<Rectangle>();
-      static List<Rectangle> overlappingOverlap = new List<Rectangle>();
-      static int[,] grid;
+    class Program
+    {
+        static List<int> claimIDs = new List<int>();
+        static List<string> lines = new List<string>();
+        static List<Rectangle> rekt = new List<Rectangle>();
+        static List<Rectangle> rekt2 = new List<Rectangle>();
+        static List<Rectangle> rekt3 = new List<Rectangle>();
+        static Dictionary<int, Rectangle> claims = new Dictionary<int, Rectangle>();
+        static Dictionary<int, int> gridNums = new Dictionary<int, int>();
+        static int[,] grid;
+        //static string[,] grid;
 
-      static int
-         largestX = 0,
-         largestY = 0;
+        static List<string>
+            freeClaimID = new List<string>();
 
-      static string _FILE = @"C:\Users\Matthew\source\repos\AOC_2018\AOC_2018\Day_3_No_Matter_How_You_Slice_It\slice.csv";
-      static void Main(string[] args)
-      {
-         using (StreamReader reader = new StreamReader(_FILE))
-         {
-            while (!reader.EndOfStream)
+        static int
+            overlapClaims = 0,
+            largestX = 0,
+            largestY = 0;
+
+        static string _FILE = @"C:\Users\Matthew\source\repos\AOC_2018\AOC_2018\Day_3_No_Matter_How_You_Slice_It\slice.csv";
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader(_FILE))
             {
-               lines.Add(reader.ReadLine());
+                while (!reader.EndOfStream)
+                {
+                    lines.Add(reader.ReadLine());
+                }
             }
-         }
 
-         foreach (string item in lines)
-         {
-            //Parse line
-            //Examples
-            // - #1 @ 1,3: 4x4
-            // - #2 @ 3,1: 4x4
-            // - #3 @ 5,5: 2x2
-            //start at 1 to skip '#'
-            string temp = item.Replace(" ", "");
-            temp = temp.Substring(1, temp.Length - 2);
-            int ind = temp.IndexOf("@");
-            int fabricNum, x, y, w, h;
-            temp = temp.Remove(0, 1);
-            int.TryParse(temp.Substring(0, 1), out fabricNum);
-            ind = temp.IndexOf("@");
-            temp = temp.Substring(ind + 1, (temp.Length - ind - 1));
-            string[] splt = temp.Split(':');
-            string[] coord = splt[0].Split(',');
-            string[] fabSize = splt[1].Split('x');
-            int.TryParse(coord[0], out x);
-            int.TryParse(coord[1], out y);
-            int.TryParse(fabSize[0].Trim(), out w);
-            int.TryParse(fabSize[1].Trim(), out h);
-            Rectangle rect = new Rectangle(x, y, w, h);
-            largestX = rect.Right > largestX ? rect.Right : largestX;
-            largestY = rect.Bottom > largestY ? rect.Bottom : largestY;
-            rekt.Add(rect);
-         }
-
-         Console.WriteLine("Largest X: " + largestX.ToString());
-         Console.WriteLine("Largest Y: " + largestY.ToString());
-         Console.WriteLine("Total sq in: " + (largestX * largestY).ToString());
-
-         //create 2D array with the largest dimensions
-         //this will be populated with Xs where an overlap exists
-         grid = new int[largestX, largestY];
-
-         for (int i = 0; i < rekt.Count; i++)
-         {
-            for (int j = i + 1; j < rekt.Count - 1; j++)
+            foreach (string item in lines)
             {
-               if (rekt[i].IntersectsWith(rekt[j]))
-               {
-                  Rectangle tempRect = rekt[i];
-                  tempRect.Intersect(rekt[j]);
-                  overlapRect.Add(tempRect);
-               }
+                //Parse line
+                //Examples
+                // - #1 @ 1,3: 4x4
+                // - #2 @ 3,1: 4x4
+                // - #3 @ 5,5: 2x2
+                //start at 1 to skip '#'
+                string temp = item.Replace(" ", "");
+                temp = item.Replace("\"", "");
+                int fabricNum, x, y, w, h;
+                temp = temp.Remove(0, 1);
+                int ind = temp.IndexOf("@");
+                int.TryParse(temp.Substring(0, ind), out fabricNum);
+                temp = temp.Substring(ind + 1, (temp.Length - ind - 1));
+                temp = temp.Trim();
+                string[] splt = temp.Split(':');
+                string[] coord = splt[0].Split(',');
+                string[] fabSize = splt[1].Split('x');
+                int.TryParse(coord[0], out x);
+                int.TryParse(coord[1], out y);
+                int.TryParse(fabSize[0].Trim(), out w);
+                int.TryParse(fabSize[1].Trim(), out h);
+                Rectangle rect = new Rectangle(x, y, w, h);
+                largestX = rect.Right > largestX ? rect.Right : largestX;
+                largestY = rect.Bottom > largestY ? rect.Bottom : largestY;
+                claims.Add(fabricNum, rect);
             }
-         }
 
-         for (int i = 0; i < overlapRect.Count; i++)
-         {
-            for (int j = overlapRect[i].X; j < overlapRect[i].Right; j++)
+            Console.WriteLine("Largest X: " + largestX.ToString());
+            Console.WriteLine("Largest Y: " + largestY.ToString());
+            Console.WriteLine("Total sq in: " + (largestX * largestY).ToString());
+
+            //create 2D array with the largest dimensions
+            //this will be populated with Xs where an overlap exists
+            grid = new string[largestX, largestY];
+
+            for (int i = 0; i < largestX; i++)
             {
-               for (int k = overlapRect[i].Y; k < overlapRect[i].Bottom; k++)
-               {
-                  grid[j, k] = 1;
-               }
+                for (int j = 0; j < largestY; j++)
+                {
+                    grid[i, j] = string.Empty;
+                }
             }
-         }
 
-         int
-            totalOverlap = 0;
-
-         for (int i = 0; i < largestX; i++)
-         {
-            for (int j = 0; j < largestY; j++)
+            //iterate through the list of all claims
+            for (int i = 1; i <= claims.Count; i++)
             {
-               totalOverlap += grid[i, j];
+                //declare a rectangle object to go through all X and Y dimensions
+                Rectangle tempRect = claims[i];
+
+                //iterate through X dimension (rows)
+                for (int j = tempRect.X; j < tempRect.Right; j++)
+                {
+                    //iterate through Y dimension (columns)
+                    for (int k = tempRect.Y; k < tempRect.Bottom; k++)
+                    {
+                        //If the grid position hasn't been populated (== string.empty)
+                        //put the claim number in that position
+                        if (grid[j, k].Equals(string.Empty))
+                        {
+                            //i = claim number
+                            grid[j, k] = i.ToString();
+                        }
+                        //if the grid position has a value (a claim number)
+                        //then replace the claim number with an "X" to show that
+                        //multiple claims overlap that grid position
+                        else
+                        {
+                            grid[j, k] = "X";
+                        }
+                    }
+                }
             }
-         }
+
+            //Iterate through the X dimension (rows)
+            for (int i = 0; i < largestX; i++)
+            {
+                //Iterate through the Y dimension (columns)
+                for (int j = 0; j < largestY; j++)
+                {
+                    string value = grid[i, j];
+                    //if the value equals X, then its an overlap
+                    //increment the counter
+                    if (value.Equals("X"))
+                    {
+                        overlapClaims++;
+                    }
+                    else
+                    {
+                        if (grid[i, j] != string.Empty)
+                        {
+                            if (!freeClaimID.Contains(grid[i, j]))
+                            {
+                                freeClaimID.Add(grid[i, j]);
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Total overlap: " + overlapClaims.ToString());
 
 
-         Console.WriteLine(string.Format("Total overlap (sq in): {0}", totalOverlap.ToString()));
-         Console.ReadLine();
-      }
+            foreach (string item in freeClaimID)
+            {
+                claimIDs.Add(int.Parse(item));
+                //rekt2.Add(claims[int.Parse(item)]);
+            }
 
-   }
+            claimIDs.Sort();
+            int timesThrough = 1;
+
+            while (claimIDs.Count > 1)
+            {
+                Find_Intersects();
+                Console.WriteLine("Looped: " + timesThrough.ToString());
+                Console.WriteLine("Size: " + claimIDs.Count.ToString());
+                timesThrough++;
+            }
+
+            Console.ReadLine();
+        }
+
+        static void Find_Intersects()
+        {
+            for (int i = 0; i < claimIDs.Count; i++)
+            {
+                Rectangle temp1 = claims[claimIDs[i]];
+                //only continue if this rectangle item hasn't intersected with another rectangle
+                for (int j = i + 1; j < claimIDs.Count; j++)
+                {
+                    Rectangle temp2 = claims[claimIDs[j]];
+                    //only continue if this rectangle item hasn't intersected with another rectangle
+                    if (temp1.IntersectsWith(temp2))
+                    {
+                        claimIDs.Remove(i);
+                        claimIDs.Remove(j);
+                        break;
+                    }
+                }
+            }
+        }
+
+    }
 
 }
